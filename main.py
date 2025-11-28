4a2c022d0bcda48b00bf681f834730bc0a6a2b8a
atrocria <220381535+mstar55@users.noreply.github.com>
2025-11-28 22:46:52 +0800

    inity

--- main.py
+++ main.py
@@ -1,59 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
 
-def convert():
-    mile_input = entry_int.get()
-    km_output = mile_input * 1.61
-    output_string.set(km_output)
-
 window = tk.Tk()
-window.title('Demo')  # fixed typo
-window.geometry('300x150')
+window.title('getting and setting widgets')
+window.geometry('800x500')
 
-style = ttk.Style()
-style.theme_use("clam")
+def func():
+    label['text'] = entry.get()
+    entry['state'] = 'disabled'
 
-# Set a global background color for the window
-window.configure(background="#ffffff")
+def reset():
+    label['text'] = 'reset'
+    entry['state'] = 'enabled'
 
-# Make ttk labels match the window
-style.configure("TLabel", background="#ffffff")
+label = ttk.Label(master=window, text='label')
+label.pack()
 
-# Make ttk frames match the window
-style.configure("TFrame", background="#ffffff")
+entry = ttk.Entry(master=window)
+entry.pack()
 
-# Make ttk entries match too
-style.configure("TEntry", fieldbackground="#ffffff", background="#ffffff")
+button = ttk.Button(master=window, text='a button to retrieve', command=func)
+button.pack()
 
-style.configure(
-    "Blue.TButton",
-    background="#fab84e",
-    foreground="black",
-    padding=6
-)
-
-style.map(
-    "Blue.TButton",
-    background=[("active", "#fab84e")],
-    foreground=[("active", "white")]
-)
-
-title_label = ttk.Label(window, text='chatbox', font='Calibri 24 bold', justify='center')
-title_label.pack()
-
-input_frame = ttk.Frame(window)
-
-entry_int = tk.IntVar()
-entry = ttk.Entry(input_frame, textvariable=entry_int)
-
-button = ttk.Button(input_frame, text='Convert', style="Blue.TButton", command=convert)
-
-entry.pack(side='left')
-button.pack(side='left')
-input_frame.pack(pady=10)
-
-output_string = tk.StringVar()
-output_label = ttk.Label(window, text='Output', font='Calibri 24', textvariable=output_string)
-output_label.pack(pady=10)
+buttonanother = ttk.Button(master=window, text='a button to reset', command=reset)
+buttonanother.pack()
 
 window.mainloop()
